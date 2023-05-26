2429f4895dcaa5f4fb6edc6f3f2728e8aef9f28d
abhijitrangari <112942964+abhijitrangari@users.noreply.github.com>
2023-05-26 12:14:56 +0530

    Add files via upload

---
+++ love calculator.py
@@ -0,0 +1,33 @@
+# input of two people for love calculator
+print("welcome to love calculator!")
+name1 = input("what is your name?\n")
+name2 = input("what is their name?\n")
+
+# combining the two names of lovers
+combined_string = name1 + name2
+lower_case_string = combined_string.lower()
+
+t = lower_case_string.count("t")
+r = lower_case_string.count("r")
+u = lower_case_string.count("u")
+e = lower_case_string.count("e")
+
+true = t + u + r + e
+
+l = lower_case_string.count("l")
+o = lower_case_string.count("o")
+v = lower_case_string.count("v")
+e = lower_case_string.count("e")
+
+love = l + o + v + e
+
+love_score = int(str(true)) + int(str(love))
+
+if (love_score < 10)  or (love_score > 90):
+    print(f"your love score is {love_score},you go together like coke and mentos.")
+elif (love_score >= 40)  or (love_score <= 50):
+    print(f"your love score is {love_score},you guys are alright together.")
+else:
+    print(f"your love score is {love_score}")
+
+
